31b2c772b09c0e6a0a148401834b03d5c0621677
seandunaway <git@seandunaway.com>
2023-06-22 20:50:15 -0700

    combine tests

--- scid.test.mjs
+++ scid.test.mjs
@@ -5,10 +5,6 @@ import * as scid from './scid.mjs'
 
 let file = './scid.test.scid'
 let fd
-let header
-let record
-let record_0
-let record_1
 
 test('open', await async function () {
     fd = await scid.open(file)
@@ -16,32 +12,33 @@ test('open', await async function () {
 })
 
 test('header', await async function () {
-    header = await scid.header(fd)
+    let header = await scid.header(fd)
     assert(header.FileTypeUniqueHeaderID == 'SCID')
 })
 
 test('record', await async function () {
-    record = await scid.record(fd)
+    let record = await scid.record(fd)
     assert(new Date(record.timestamp))
     assert(record.High > record.Low)
-})
 
-test('record_0', await async function () {
-    record_0 = await scid.record(fd, 0)
+    let record_0 = await scid.record(fd, 0)
     assert.deepEqual(record, record_0)
-})
 
-test('record_1', await async function () {
-    record_1 = await scid.record(fd, 1)
+    let record_1 = await scid.record(fd, 1)
     assert.notDeepEqual(record_0, record_1)
 })
 
 test('records', await async function () {
     let records = []
     for await (let record of scid.records(fd, 2, 4, 2)) {
-        records.push(2)
+        records.push(record)
     }
     assert(records.length == 2)
+
+    let records_undefined = []
+    for await (let record of scid.records(fd)) {
+        records_undefined.push(record)
+    }
 })
 
 test('records_quantity', await async function () {
